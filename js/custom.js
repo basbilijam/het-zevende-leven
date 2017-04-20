444ef5b161c89be7270e0009b432cf902908711b
basbilijam <basbilijam@gmail.com>
2017-04-20 10:43:06 +0200

    whole website ready for deployment

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  // no automatic zoom-in when scrolling over the GoogleMaps iframe
+  $('.map-container').click(function () {
+    $('.map-container iframe').css("pointer-events", "auto");
+    });
+
+  $( ".map-container" ).mouseleave(function() {
+    $('.map-container iframe').css("pointer-events", "none");
+  });
+});
---
+++ readMe.md
@@ -0,0 +1,15 @@
+# B&B website plan
+
+1. Make design
+  - Online wireframe programme or draw
+2. Find and collect images and texts
+  - store them on computer for later use
+3. Start building
+  - use bootstrap
+  - example Kupa
+4. Register domain on GoDaddy
+5. Host website on own digitalocean droplet
+
+# Time-frame
+
+Finished by Friday 21 April
